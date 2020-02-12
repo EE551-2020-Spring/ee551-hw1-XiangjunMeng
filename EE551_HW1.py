5ff064afec46e13fddfa385b9733fa834aba7753
XiangjunMeng <47197111+XiangjunMeng@users.noreply.github.com>
2020-02-11 23:42:39 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -14,14 +14,19 @@ def numbers_and_strings():
     x = "EE551"
 
     # Assign a string "Stevens" to a variable y
+    y="Stevens"
 
     # Repeat variable y 5 times
+    z=y*5
 
     # What is the length of z?
+    length=len(z)
 
     # Concatenate variable y with string " is good"
+    m=y+"is good"
 
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
+    n=m.replace("good","awesome")
 
     return x, y, z, length, m, n
 
@@ -33,21 +38,27 @@ def lists():
     n = "Stevens is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
+    p=n.split(' ')
 
     # Get all the items past the first of the third substring
+    r=p[-1]
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
+    A=[[1,4,5],[6,10,11],[12,17,38]]
 
     # Collect the items in the last column of matrix A using list comprehension
+    c=[a[2] for a in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
-
+    d=[a[i] for a in A for i in range(3)][::4][1:3]
+    
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
+    o=list(map(lambda x:ord(x),'Stevens'))
 
     return p, r, c, d, o
 
@@ -65,6 +76,9 @@ def dictionaries():
 
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
+    a={"fruit":"apple","quantity":"4","color":"green"}
+    f=a["fruit"]
+    a["quantity"]+=1
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
@@ -73,10 +87,14 @@ def dictionaries():
 
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
+    p={"name":{"first_name":"Grace","last_name":"Hopper"},"jobs":["scientist","engineer"],"age":85}
+    p["jobs"].append("programmar")
 
     # Get the third job Grace has that you recently added
+    k=p["jobs"][2]
 
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
+    sorted(p)
 
     return a, f, p, k
 
